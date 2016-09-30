6a2e7c2a442a7f8bded748991ec18b04afeb4733
bc10000 <fyousif1@mail.csuchico.edu>
2016-09-29 20:07:49 -0700

    added my C# scripts

---
+++ Fadi_Folder/Fan.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fan : MonoBehaviour
+{
+    public float FanSpeed = 3.0f;     // delaring a public variable for the speed of rotation
+
+void Start(){
+        transform.Rotate(0, 0, 0);
+    }//Start
+//------------------------------------------------------------------- 
+//This update function gets called once every frame.
+void Update(){
+        transform.Rotate(0, 0, FanSpeed); // (x,y,z) thus we are updating the z position every sec
+    }//Update
+
+
+}//Fan class
---
+++ Fadi_Folder/Reticle.cs
@@ -0,0 +1,62 @@
+﻿/*
+Reticles:
+	Drawing a reticle in unity development kit was a fair challenge. 
+    The obvious thinking is to simply create a crosshair that will track your head movement, 
+    by doing that then as your head moves left the crosshair will follow, and vice versa. However, 
+    when you look at different planes in environment then your eyes will adjust themselves 
+    probably to account for that depth, and if your reticle cannot adapt to the depth of 
+    field changes then it might be perceived as double image. In order for the 
+    reticle to work properly, it must be redrawn directly into the object the user is looking at.
+Steps
+1.	Create a PNG transparent image http://www190.lunapic.com/editor/
+2.	Import the image and select alpha is transparency.
+3.	Create a new Material in the Environment 
+4.	For the material under shader select unlit/Transparent.
+5.	Attach the picture as texture to the material.
+*/
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour {
+
+    public Camera CameraFacing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float distance;
+
+        // we will identify the closest object the camera is looking at so that we may
+        // draw the reticle correctly on the closest object. this will prevent the double image effect.
+        /*
+        RaycastHit hit;
+        if (Physics.Raycast(new Ray(CameraFacing.transform.position,
+                                    CameraFacing.transform.rotation * Vector3.forward), out hit))
+        {
+            distance = hit.distance;
+        }//if
+        else { distance = CameraFacing.farClipPlane * 0.95f; }
+        */
+
+        // Vector3(0.1f, 0.1f, 0.1f) will allow us the scale the crosshair linearly as the distance changes.
+        // longer distances = bigger cross hair, closer distances = smalled cross hair.
+        //transform.localScale = new Vector3(0.1f, 0.1f, 0.1f) * distance;
+
+        transform.LookAt(CameraFacing.transform.position); // this will allow the reticle to face the position 
+                                                           //of the camera in the world
+        transform.Rotate(0.0f, 180.0f, 0.0f);              // allow the reticle to rotate around the y axis 
+                                                           // in order to have it always facing the camera
+
+        // now we will make the reticle follow the camera forward facing vector in order to have the reticle
+        // move with respect to your head movement.
+        transform.position = CameraFacing.transform.position +
+            (CameraFacing.transform.rotation * Vector3.forward * 0.5f);
+
+    }//update
+}
