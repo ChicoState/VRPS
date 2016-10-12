55f85fc6852ffa206d424a477df42878d515f2a7
Luke Short <lshort2@mail.csuchico.edu>
2016-10-12 04:34:59 -0700

    Man vs. Android pathing. Man wins.

---
+++ LukeShort/myScripts/loadimageFromAndroidFilePath.cs
@@ -0,0 +1,26 @@
+﻿//Fixed and very much working method to access com.productName.appName folder on android
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class loadimageFromAndroidFilePath : MonoBehaviour {
+
+	// Use this for initialization
+	// Don't want to use a void here, because we need to be able to yield
+	//   when we "download" the texture
+	IEnumerator Start ()
+	{
+		string myPath = Application.persistentDataPath;
+		string imagePath = "file://" + myPath + "/img1.png";
+		WWW myUrl = new WWW (imagePath);
+		yield return myUrl;
+
+		Renderer myRend = GetComponent<Renderer> ();
+		myRend.material.mainTexture = myUrl.texture;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
---
+++ LukeShort/myScripts/writeStreamingAssetsPathToText.cs
@@ -0,0 +1,23 @@
+﻿//used this to print to screen and double check file path working on phone
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class writeStreamingAssetsPathToText : MonoBehaviour
+{
+	private Text myText;
+	// Use this for initialization
+	void Start ()
+	{
+		myText = GetComponent<Text> ();
+		//Application.streamingAssetsPath comes up w/
+		//this comes out to be "jar:file:///data/app/com.VRPS.myProductName-1/base.apk!/assets"
+		myText.text = Application.persistentDataPath;
+		//comes up w/ /storage/emulated/0/Android/data/com.VRPS.myProductName/files
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
