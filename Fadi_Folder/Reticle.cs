f6b3ed00a4ae045fc9980ea96aaa4e07ae3af711
bc10000 <fyousif1@mail.csuchico.edu>
2016-11-09 22:51:22 -0800

    latest and gratest

--- Fadi_Folder/Reticle.cs
+++ Fadi_Folder/Reticle.cs
@@ -20,48 +20,77 @@ using UnityEngine;
 using System.Collections;
 
 public class Reticle : MonoBehaviour {
-    private float defaultPosZ;
-    private Vector3 Reticle_Scale;
     public Camera CameraFacing;
 
-	// Use this for initialization
-	void Start () {
+     // the obejct that controls when the reticle shows up on the screen
+    private MeshRenderer MeshRendObj { get { return GetComponent<MeshRenderer>(); } }
+
+    private bool stair_to_select;
+    private float defaultPosZ;
+    private Vector3 Reticle_Scale;
+
+    // Use this for initialization
+    void Start () {
         Reticle_Scale = transform.localScale; // getting the original scale of the reticle at start up, this can be changed from the GUI
         defaultPosZ = transform.localPosition.z;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        float distance;
 
+        MeshRendObj.enabled = false; // at the start of the scene, the reticle is disabled from view. 
+    }
+
+    public void Rotate_Off()
+    {
+        stair_to_select = false;
+        MeshRendObj.enabled = false; // When an object is Unhighlighed/Clicked allow the Reticle to disappear.
+    }
+
+    public void Rotate_On()
+    {
+        stair_to_select = true;
+        MeshRendObj.enabled = true; // When an object is highlighted allow the Reticle to appear.
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+        float distance;
         // we will identify the closest object the camera is looking at so that we may
         // draw the reticle correctly on the closest object. this will prevent the double image effect.
-           
+
         RaycastHit hit;
         Ray ray = new Ray(CameraFacing.transform.position,
-                          CameraFacing.transform.rotation * Vector3.forward);
+                            CameraFacing.transform.rotation * Vector3.forward);
 
         if (Physics.Raycast(ray, out hit))
         {
             distance = hit.distance; // when the ray collides with an object it will set the distance to the objects distance from the camera.
 
-    }//if
+        }//if
         else { distance = defaultPosZ; }// incase the ray doesnt hit a thing
 
 
         //Vector3(0.1f, 0.1f, 0.1f) will allow us the scale the crosshair linearly as the distance changes.
         // longer distances = bigger cross hair, closer distances = smaller cross hair.
-        transform.localScale = Reticle_Scale * distance;
+        transform.localScale = Reticle_Scale * 1.5f;
 
-        transform.LookAt(CameraFacing.transform.position); // this will allow the reticle to face the position 
-                                                           //of the camera in the world
-        transform.Rotate(0.0f, 180.0f, 0.0f);              // allow the reticle to rotate around the y axis 
-                                                           // in order to have it always facing the camera
+        if (!stair_to_select)
+        {
+            transform.LookAt(CameraFacing.transform.position); //this will allow the reticle to face the position 
+                                                               //of the camera in the world
+            transform.Rotate(0.0f, 180.0f, 0.0f);              // allow the reticle to rotate around the y axis 
+                                                               // in order to have it always facing the camera  
+        }
+        else {
+
+            transform.Rotate(0.0f, 0.0f, 8.0f);                 // if stop_looking = true, then stop making the reticle
+                                                                // look at the camera and allow it to rotate about the z-axis.
+        }
 
         // now we will make the reticle follow the camera forward facing vector in order to have the reticle
         // move with respect to your head movement.
         transform.position = CameraFacing.transform.position +
-            (CameraFacing.transform.rotation * Vector3.forward * distance);
+            (CameraFacing.transform.rotation * Vector3.forward * 1.0f);
 
     }//update
-}
+    
+}//class
+
---
+++ Fadi_Folder/TestingNativeDLL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+//the extension .dll can only be used if the DLL is placed in the root directory of the project, 
+//otherwise if the DLL is placed anywhere inside the asset folder or its subdirectories (ex plugins) 
+//then the .dll must be removed.
+public class TestingNativeDLL : MonoBehaviour {
+    [DllImport("SampleCW")] 
+        private static extern int add(int a, int b);
+    //[DllImport("SampleC2")]
+    //    private static extern void Calling(Action<int> action);
+
+    ////pointers using safe code
+    ////replace SharedAPI* with IntPtr
+    //[DllImport("SampleC2")]
+    //    private static extern IntPtr PtrFunction(int id);// creates a constructor and defines ID value
+    //[DllImport("SampleC2")]
+    //    private static extern void CleanMem(IntPtr api, IntPtr Sptr);
+    //[DllImport("SampleC2")]
+    // private static extern int Print(IntPtr api);
+    //[DllImport("SampleC2")]
+    //    private static extern IntPtr structPtr();
+    //[DllImport("SampleC2")]
+    //    private static extern int GetHeight(IntPtr Spt);
+
+    //private GameObject SphereOne;
+    //private Material sphereMaterial;
+    Text text;
+    //IntPtr SharedPTR; // C# pointer 
+    //IntPtr StructurePTR;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        //Calling(Icalled);
+        ////sphereMaterial = SphereOne.GetComponent<Renderer>().material;
+        //SharedPTR = PtrFunction(17);
+        //StructurePTR = structPtr();
+    }
+
+    private void Icalled(int value)
+    {
+        //text.text = value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //text.text = "Hello";
+        text.text = add(65, 6).ToString();
+        // sphereMaterial.mainTexture= 
+        //text.text = Print(SharedPTR).ToString();
+        //text.text = GetHeight(StructurePTR).ToString();
+    }
+
+    //public void OnDestroy()
+    //{
+    //    CleanMem(SharedPTR, StructurePTR);
+    //}
+}
+
